8f7d6b1f4f58c8c1c451de35ba4bda1d6357ca5f
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-26 16:39:06 +0700

    Solved 11

---
+++ Exercise 11: Reverse a given string.py
@@ -0,0 +1,18 @@
+# Exercise 11: Reverse a given string
+
+# Given:
+
+# str1 = "PYnative"
+
+# Expected Output:
+
+# evitanYP
+
+
+def main():
+    string:str = "PYnative"
+    print(string[::-1])
+    #print(''.join(reversed(string)))
+
+if __name__=="__main__":
+    main()
